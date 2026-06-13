f45b44528a828d3c475a377edef8c0e5f040b505
Ruben <rubennugmanov734@gmail.com>
2026-06-13 23:25:03 +0400

    Update MainActivity.kt

--- app/src/main/java/com/example/woompy/MainActivity.kt
+++ app/src/main/java/com/example/woompy/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.woompy
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import androidx.activity.compose.setContent 
 import com.example.woompy.ui.theme.WoompyTheme
 
 class MainActivity : ComponentActivity() {
